f0d8b2e3bfbe30549a7b934e08bd661ce13dcad9
cdx <chendaxin@lidig.com>
2020-11-26 09:49:38 +0800

    fix: 调试

--- git.test/src/main/java/DemoController.java
+++ git.test/src/main/java/DemoController.java
@@ -1,6 +1,6 @@
 public class DemoController {
     public static void main(String[] args) {
-        System.out.print("Hello Word");
+        System.out.print("hahaha");
 
         System.out.print("hahaha");
     }
